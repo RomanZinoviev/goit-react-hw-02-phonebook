69b2ab7b7f780e41aed58e3e10d93974eb6e3955
RomanZinoviev <rzinovijev@gmail.com>
2022-05-05 21:24:26 +0300

    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component{
+  state = {
+  contacts: [],
+  name: ''
+  }
+
+  
+  render() {
+    return (
+      <label htmlFor="name">Name
+        <input
+          value={this.state.name}
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+      </label>
+    )
+  }
+}
